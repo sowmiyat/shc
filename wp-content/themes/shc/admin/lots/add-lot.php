5e049d164ff49deab3cc4c73166a67bc24c3f833
seegan <see.gan21@gmail.com>
2017-10-19 18:26:48 +0530

    tab changes

--- wp-content/themes/shc/admin/lots/add-lot.php
+++ wp-content/themes/shc/admin/lots/add-lot.php
@@ -17,7 +17,7 @@ if(isset($_GET['id']) && $lot = get_lot($_GET['id']) ) {
 ?>
 <div class="container">
 
-	<div class="row">
+	<div class="row" style="margin-top:20px;">
 		<div class="col-lg-6 col-lg-offset-3 col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2">
 			<div class="x_panel">
 				<div class="x_title">
@@ -26,78 +26,105 @@ if(isset($_GET['id']) && $lot = get_lot($_GET['id']) ) {
 				</div>
 				<div class="x_content">
 					<form class="form-horizontal form-label-left" id="create_lot">
-						<div class="form-group">
-							<!-- <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Lot Number <span class="required">*</span>
-							</label> -->
+						<input type="hidden" id="lot_no" name="lot_no" required="required" class="form-control col-md-7 col-xs-12" autocomplete="off" value="<?php echo ($lot) ? $lot->lot_no : '0'; ?>">
+
+
+
+
+
+						<div class="row">
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="hidden" id="lot_no" name="lot_no" required="required" class="form-control col-md-7 col-xs-12" autocomplete="off" value="<?php echo ($lot) ? $lot->lot_no : '0'; ?>">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">Brand Name <span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<input type="text" id="brand_name" name="brand_name" required="required" class="form-control col-md-7 col-xs-12 unique_brand" autocomplete="off" value="<?php echo ($lot) ? $lot->brand_name : ''; ?>">
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">Product Name <span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<input type="text" id="product_name" name="product_name" required="required" class="form-control col-md-7 col-xs-12 unique_product"  autocomplete="off" value="<?php echo ($lot) ? $lot->product_name : ''; ?>">
+									</div>
+								</div>
 							</div>
 						</div>
+
 						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Brand Name <span class="required">*</span>
-							</label>
+
+						<div class="row">
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="brand_name" name="brand_name" required="required" class="form-control col-md-7 col-xs-12 unique_brand" autocomplete="off" value="<?php echo ($lot) ? $lot->brand_name : ''; ?>">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">Selling Price<span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<input type="text" id="selling_price" name="selling_price"  required="required" class="form-control col-md-7 col-xs-12" onkeypress="return isNumberKey(event)" autocomplete="off" value="<?php echo ($lot) ? $lot->selling_price : ''; ?>">
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">Purchase Price <span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<input type="text" id="purchase_price" name="purchase_price"  required="required" class="form-control col-md-7 col-xs-12" onkeypress="return isNumberKey(event)" autocomplete="off" value="<?php echo ($lot) ? $lot->purchase_price : ''; ?>">
+									</div>
+								</div>
 							</div>
 						</div>
+
 						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Product Name <span class="required">*</span>
-							</label>
+
+						<div class="row">
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="product_name" name="product_name" required="required" class="form-control col-md-7 col-xs-12 unique_product"  autocomplete="off" value="<?php echo ($lot) ? $lot->product_name : ''; ?>">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">CGST %<span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<select id="cgst" name="cgst">
+											<option value="0.00" <?php if(($lot->cgst) == '0.00'){ echo 'selected'; } ?>>0.0%</option>
+											<option value="2.50" <?php if(($lot->cgst) == '2.50'){ echo 'selected'; } ?>>2.50%</option>
+											<option value="6.00" <?php if(($lot->cgst) == '6.00'){ echo 'selected'; } ?>>6.00%</option>
+											<option value="9.00" <?php if(($lot->cgst) == '9.00'){ echo 'selected'; } ?>>9.00%</option>
+											<option value="14.00" <?php if(($lot->cgst) == '14.00'){ echo 'selected'; } ?>>14.00%</option>
+										</select>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-6 col-sm-6 col-xs-12">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">SGST % <span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<input type="text" id="sgst" name="sgst" required="required" class="form-control col-md-7 col-xs-12" readonly  value="<?php echo ($lot) ? $lot->sgst : ''; ?>">
+									</div>
+								</div>
 							</div>
 						</div>
+
 						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Selling Price<span class="required">*</span>
-							</label>
+
+
+
+						<div class="row">
 							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="selling_price" name="selling_price"  required="required" class="form-control col-md-7 col-xs-12" onkeypress="return isNumberKey(event)" autocomplete="off" value="<?php echo ($lot) ? $lot->selling_price : ''; ?>">
+								<div class="form-group">
+									<label class="control-label col-md-6 col-sm-6 col-xs-12" for="first-name">HSN Code <span class="required">*</span>
+									</label>
+									<div class="col-md-6 col-sm-6 col-xs-12">
+										<input type="text" id="hsn" name="hsn" required="required" class="form-control col-md-7 col-xs-12" autocomplete="off"  onkeypress="return isNumberKey(event)" value="<?php echo ($lot) ? $lot->hsn : ''; ?>">
+									</div>
+								</div>
 							</div>
 						</div>
+
+						
 						<div class="divider-dashed"></div>
 						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Purchase Price <span class="required">*</span>
-							</label>
-							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="purchase_price" name="purchase_price"  required="required" class="form-control col-md-7 col-xs-12" onkeypress="return isNumberKey(event)" autocomplete="off" value="<?php echo ($lot) ? $lot->purchase_price : ''; ?>">
-							</div>
-						</div>
-						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">CGST %<span class="required">*</span>
-							</label>
-							<div class="col-md-6 col-sm-6 col-xs-12">
-								<select id="cgst" name="cgst">
-									<option value="0.00" <?php if(($lot->cgst) == '0.00'){ echo 'selected'; } ?>>0.0%</option>
-									<option value="2.50" <?php if(($lot->cgst) == '2.50'){ echo 'selected'; } ?>>2.50%</option>
-									<option value="6.00" <?php if(($lot->cgst) == '6.00'){ echo 'selected'; } ?>>6.00%</option>
-									<option value="9.00" <?php if(($lot->cgst) == '9.00'){ echo 'selected'; } ?>>9.00%</option>
-									<option value="14.00" <?php if(($lot->cgst) == '14.00'){ echo 'selected'; } ?>>14.00%</option>
-								</select>
-							</div>
-						</div>
-						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">SGST % <span class="required">*</span>
-							</label>
-							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="sgst" name="sgst" required="required" class="form-control col-md-7 col-xs-12" readonly  value="<?php echo ($lot) ? $lot->sgst : ''; ?>">
-							</div>
-						</div>
-						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">HSN Code <span class="required">*</span>
-							</label>
-							<div class="col-md-6 col-sm-6 col-xs-12">
-								<input type="text" id="hsn" name="hsn" required="required" class="form-control col-md-7 col-xs-12" autocomplete="off"  onkeypress="return isNumberKey(event)" value="<?php echo ($lot) ? $lot->hsn : ''; ?>">
-							</div>
-						</div>
-						<div class="divider-dashed"></div>
-						<div class="form-group">
-							<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+							<div style="text-align:center;">
 								<button type="submit" class="btn btn-success sub_form">Submit</button>
 	                          	<button class="btn btn-primary" type="button" onclick="window.location = '<?php echo admin_url('admin.php?page=list_lots'); ?>';">Cancel</button>
 							  	<button class="btn btn-primary reset_button" type="reset" >Reset</button>
